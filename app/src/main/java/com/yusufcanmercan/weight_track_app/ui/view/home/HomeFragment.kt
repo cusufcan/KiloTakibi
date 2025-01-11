6f2b8bf5c23bd5365e88cdc11aa609b603442580
cusufcan <cusufcan@gmail.com>
2025-01-11 19:07:16 +0300

    feat: display weight dummy data in HomeFragment

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
@@ -5,17 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.yusufcanmercan.weight_track_app.data.model.Weight
 import com.yusufcanmercan.weight_track_app.databinding.FragmentHomeBinding
 import com.yusufcanmercan.weight_track_app.ui.adapter.WeightAdapter
-import kotlin.random.Random
+import com.yusufcanmercan.weight_track_app.ui.state.WeightUIState
+import com.yusufcanmercan.weight_track_app.ui.viewmodel.WeightViewModel
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var recyclerView: RecyclerView
+
+    private val weightViewModel: WeightViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -27,17 +33,32 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bindViews()
+        observeData()
+    }
 
-        val weights = mutableListOf<Weight>()
-        repeat(50) {
-            val weight = Random.nextDouble() * 100.0
-            val date = "${Random.nextInt(30 + 1)}.${Random.nextInt(12) + 1}.2024"
-
-            weights.add(Weight(weight, date))
+    private fun observeData() {
+        lifecycleScope.launch {
+            weightViewModel.weights.collect {
+                when (it) {
+                    is WeightUIState.Loading -> loadingLogic()
+                    is WeightUIState.Success -> successLogic(it.weights)
+                    is WeightUIState.Error -> errorLogic()
+                }
+            }
         }
+    }
 
+    private fun loadingLogic() {
+
+    }
+
+    private fun successLogic(weights: List<Weight>) {
         val adapter = WeightAdapter(weights)
         recyclerView.adapter = adapter
+    }
+
+    private fun errorLogic() {
+
     }
 
     private fun bindViews() {
