70eddea5c8c24acafc046648a9ce7279cc31c61f
cusufcan <cusufcan@gmail.com>
2025-01-11 13:13:06 +0300

    add: GraphFragment

---
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/graph/GraphFragment.kt
@@ -0,0 +1,25 @@
+package com.yusufcanmercan.weight_track_app.ui.view.graph
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yusufcanmercan.weight_track_app.databinding.FragmentGraphBinding
+
+class GraphFragment : Fragment() {
+    private var _binding: FragmentGraphBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGraphBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
