73dc77dc1332cfe89bc91c0dc5ecec698ca50d54
cusufcan <cusufcan@gmail.com>
2025-01-11 18:38:34 +0300

    feat: display dummy weights in recyclerview

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
@@ -5,17 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.yusufcanmercan.weight_track_app.data.model.Weight
 import com.yusufcanmercan.weight_track_app.databinding.FragmentHomeBinding
+import com.yusufcanmercan.weight_track_app.ui.adapter.WeightAdapter
+import kotlin.random.Random
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews()
+
+        val weights = mutableListOf<Weight>()
+        repeat(50) {
+            val weight = Random.nextDouble() * 100.0
+            val date = "${Random.nextInt(30 + 1)}.${Random.nextInt(12) + 1}.2024"
+
+            weights.add(Weight(weight, date))
+        }
+
+        val adapter = WeightAdapter(weights)
+        recyclerView.adapter = adapter
+    }
+
+    private fun bindViews() {
+        recyclerView = binding.recyclerView
     }
 
     override fun onDestroyView() {
