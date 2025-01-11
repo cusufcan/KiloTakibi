c6caed96ff3ee392ec21a953275ac31bd0f7509a
cusufcan <cusufcan@gmail.com>
2025-01-11 13:26:02 +0300

    add: bind bottom navigation view and navigation

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/main/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.yusufcanmercan.weight_track_app.R
 import com.yusufcanmercan.weight_track_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var fragmentContainerView: FragmentContainerView
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingCodes()
         defaultActivityCodes()
+        bindViews()
+        bindEvents()
     }
 
     private fun bindingCodes() {
@@ -29,5 +39,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun bindViews() {
+        fragmentContainerView = binding.fragmentContainerView
+        navHostFragment = supportFragmentManager.findFragmentById(
+            fragmentContainerView.id
+        ) as NavHostFragment
+
+        bottomNavigationView = binding.bottomNavigationView
+    }
+
+    private fun bindEvents() {
+        bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
 }
