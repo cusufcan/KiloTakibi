1961068184b47f371c1e6a62502152a70ee8ed9e
cusufcan <cusufcan@gmail.com>
2025-01-22 17:09:57 +0300

    refactor: remove redundant bottom sheet dialog configurations

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/add/AddFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/add/AddFragment.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.yusufcanmercan.weight_track_app.databinding.FragmentAddBinding
 
@@ -23,24 +21,9 @@ class AddFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        dialog?.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-        dialog?.window?.setFlags(
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-        )
+        bindViews()
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        val dialog = dialog as? BottomSheetDialog
-        val bottomSheet =
-            dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-        bottomSheet?.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
+    private fun bindViews() {
     }
-
 }
