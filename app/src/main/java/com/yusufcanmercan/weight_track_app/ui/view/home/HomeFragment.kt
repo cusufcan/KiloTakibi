ef3bc8ff2a3712c17ee1e68c9bd5155112278bf8
cusufcan <cusufcan@gmail.com>
2025-01-24 21:47:18 +0300

    feat: display loading state, empty state and error state in HomeFragment

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/home/HomeFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import com.yusufcanmercan.weight_track_app.R
 import com.yusufcanmercan.weight_track_app.data.model.Weight
 import com.yusufcanmercan.weight_track_app.databinding.FragmentHomeBinding
 import com.yusufcanmercan.weight_track_app.ui.adapter.WeightAdapter
@@ -19,12 +24,16 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var progressBar: ProgressBar
     private lateinit var recyclerView: RecyclerView
+    private lateinit var etEmpty: MaterialTextView
+
+    private lateinit var navController: NavController
 
     private val weightViewModel: WeightViewModel by activityViewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
@@ -32,8 +41,26 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bindVariables()
         bindViews()
+        bindEvents()
         observeData()
+    }
+
+    private fun bindVariables() {
+        navController = findNavController()
+    }
+
+    private fun bindViews() {
+        progressBar = binding.progressBar
+        recyclerView = binding.recyclerView
+        etEmpty = binding.etEmpty
+    }
+
+    private fun bindEvents() {
+        etEmpty.setOnClickListener {
+            navController.navigate(R.id.action_homeFragment_to_addFragment)
+        }
     }
 
     private fun observeData() {
@@ -42,27 +69,39 @@ class HomeFragment : Fragment() {
                 when (it) {
                     is WeightUIState.Loading -> loadingLogic()
                     is WeightUIState.Success -> successLogic(it.weights)
-                    is WeightUIState.Error -> errorLogic()
+                    is WeightUIState.Error -> errorLogic(it.message)
                 }
             }
         }
     }
 
     private fun loadingLogic() {
-
+        progressBar.visibility = View.VISIBLE
+        recyclerView.visibility = View.GONE
+        etEmpty.visibility = View.GONE
     }
 
     private fun successLogic(weights: List<Weight>) {
+        progressBar.visibility = View.GONE
+
+        if (weights.isEmpty()) {
+            etEmpty.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        } else {
+            etEmpty.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+        }
+
         val adapter = WeightAdapter(weights)
         recyclerView.adapter = adapter
     }
 
-    private fun errorLogic() {
+    private fun errorLogic(message: String) {
+        progressBar.visibility = View.GONE
+        recyclerView.visibility = View.GONE
+        etEmpty.visibility = View.VISIBLE
 
-    }
-
-    private fun bindViews() {
-        recyclerView = binding.recyclerView
+        etEmpty.text = message
     }
 
     override fun onDestroyView() {
