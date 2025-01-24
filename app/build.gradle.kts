41a799f5d250bb3ddbca541bdae55a9ef881771d
cusufcan <cusufcan@gmail.com>
2025-01-24 21:49:08 +0300

    feat: enable minification, shrinkResources and signing for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,10 +21,12 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
