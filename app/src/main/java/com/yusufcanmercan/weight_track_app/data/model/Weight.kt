0a07f7f6b89ae302655c10e5d66bfab06c7b4059
cusufcan <cusufcan@gmail.com>
2025-01-24 22:08:28 +0300

    feat: add Entity and PrimaryKey annotations to Weight data class

--- app/src/main/java/com/yusufcanmercan/weight_track_app/data/model/Weight.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/data/model/Weight.kt
@@ -1,8 +1,13 @@
 package com.yusufcanmercan.weight_track_app.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Weight(
     val weight: Double,
     val date: String,
 ) {
+    @PrimaryKey(autoGenerate = true)
     var id = 0
 }
