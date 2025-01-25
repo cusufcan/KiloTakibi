56a12b45cb001037cc569692a1b440970dc90111
cusufcan <cusufcan@gmail.com>
2025-01-25 16:13:23 +0300

    feat: move and edit EditFragment

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/edit/EditFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/edit/EditFragment.kt
@@ -1,10 +1,11 @@
-package com.yusufcanmercan.weight_track_app.ui.view
+package com.yusufcanmercan.weight_track_app.ui.view.edit
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.yusufcanmercan.weight_track_app.data.model.Weight
 import com.yusufcanmercan.weight_track_app.databinding.FragmentEditBinding
 
 class EditFragment : DialogFragment() {
@@ -17,6 +18,16 @@ class EditFragment : DialogFragment() {
     ): View {
         _binding = FragmentEditBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bundle = arguments
+        if (bundle != null) {
+            val weight = bundle.getParcelable<Weight>("weight")
+            binding.text.text = weight?.weight.toString()
+        }
     }
 
     override fun onDestroyView() {
