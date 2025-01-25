da576795dcda339c9aead0a00bdcf47d0029207e
cusufcan <cusufcan@gmail.com>
2025-01-25 16:40:39 +0300

    feat: create Constants file for SimpleDateFormat

---
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.yusufcanmercan.weight_track_app.core
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+object Constants {
+    val locale = Locale("tr", "TR")
+    val formatter = SimpleDateFormat("dd MMMM yyyy", locale)
+}
