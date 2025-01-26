4e3789c1c980d53d0b0fcf47b5d756d82e8f759c
cusufcan <cusufcan@gmail.com>
2025-01-27 00:03:45 +0300

    feat: add WeightStat data class

---
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/data/model/WeightStat.kt
@@ -0,0 +1,8 @@
+package com.yusufcanmercan.weight_track_app.data.model
+
+data class WeightStat(
+    val current: Double? = null,
+    val change: Double? = null,
+    val weekly: Double? = null,
+    val monthly: Double? = null,
+)
