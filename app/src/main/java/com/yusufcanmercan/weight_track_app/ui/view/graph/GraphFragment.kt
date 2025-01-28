45c70109bc48753d76703ac4aed70acf9e02f539
cusufcan <cusufcan@gmail.com>
2025-01-28 16:02:30 +0300

    feat: implement MaterialButtonToggleGroup for graph view

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/graph/GraphFragment.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/graph/GraphFragment.kt
@@ -5,17 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import com.yusufcanmercan.weight_track_app.databinding.FragmentGraphBinding
 
 class GraphFragment : Fragment() {
     private var _binding: FragmentGraphBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var btn1W: MaterialButton
+    private lateinit var btn1M: MaterialButton
+    private lateinit var btn3M: MaterialButton
+    private lateinit var btn1Y: MaterialButton
+    private lateinit var btn3Y: MaterialButton
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentGraphBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews()
+        bindEvents()
+        setStartDefaults()
+    }
+
+    private fun bindViews() {
+        btn1W = binding.btn1W
+        btn1M = binding.btn1M
+        btn3M = binding.btn3M
+        btn1Y = binding.btn1Y
+        btn3Y = binding.btn3Y
+    }
+
+    private fun setStartDefaults() {
+        btn1W.isChecked = true
+    }
+
+    private fun bindEvents() {
+        btn1W.setOnClickListener {}
+        btn1M.setOnClickListener {}
+        btn3M.setOnClickListener {}
+        btn1Y.setOnClickListener {}
+        btn3Y.setOnClickListener {}
     }
 
     override fun onDestroyView() {
