7f981af4428c5fcb8b20412f685693e07f7d41da
cusufcan <cusufcan@gmail.com>
2025-01-29 14:16:03 +0300

    feat: add MPAndroidChart library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,7 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // MPAndroidChart
+    implementation(libs.mpandroidchart)
 }
