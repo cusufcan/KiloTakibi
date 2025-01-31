29390e148aca9ed9ab3127b921d43ef28252058f
cusufcan <cusufcan@gmail.com>
2025-02-01 01:37:34 +0300

    feat: format dates on the graph

--- app/src/main/java/com/yusufcanmercan/weight_track_app/util/graph/DateValueFormatter.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/util/graph/DateValueFormatter.kt
@@ -5,6 +5,7 @@ import com.yusufcanmercan.weight_track_app.core.Constants
 
 class DateValueFormatter(private val dates: Array<String>) : ValueFormatter() {
     override fun getFormattedValue(value: Float): String {
+        if (value < 0 || value >= dates.size) return ""
         val date = dates[value.toInt()]
         val formattedDate = Constants.formatter.parse(date)!!
         val formattedString = Constants.formatterGraph.format(formattedDate)
