31457bd36bf3179ede46e9e28535bf4d9d56d61d
cusufcan <cusufcan@gmail.com>
2025-02-20 17:17:27 +0300

    refactor: set onApplyWindowInsetsListener on root view

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/activity/main/MainActivity.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/activity/main/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun defaultActivityCodes() {
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
