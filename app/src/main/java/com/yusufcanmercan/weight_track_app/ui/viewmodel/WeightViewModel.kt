f9c57ca036e6cfcf44b87866564672e495c3427c
cusufcan <cusufcan@gmail.com>
2025-03-10 12:14:00 +0300

    fix: handle IOException in WeightViewModel
    This commit addresses a potential `IOException` that could occur within the `WeightViewModel` when fetching weight data. The change ensures that `IOException` is specifically caught and handled, rather than catching any `Exception`.
    
    - Modified the `try-catch` block in `getWeights` function to catch `IOException` instead of the generic `Exception`.

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/viewmodel/WeightViewModel.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/viewmodel/WeightViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.IOException
 import java.util.Date
 import javax.inject.Inject
 
@@ -35,7 +36,7 @@ class WeightViewModel @Inject constructor(
 
                 _weights.value = WeightUIState.Success(weights)
                 _weightStat.value = weightStat
-            } catch (e: Exception) {
+            } catch (e: IOException) {
                 _weights.value = WeightUIState.Error(e.message ?: "Unknown error")
             }
         }
