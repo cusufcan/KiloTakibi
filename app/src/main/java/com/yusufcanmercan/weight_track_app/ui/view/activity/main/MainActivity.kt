ec6d835b98216da3a3fe16eaf5bfaccb9e32f2e5
cusufcan <cusufcan@gmail.com>
2025-07-17 13:41:23 +0300

    feat: enable edge-to-edge display and adjust padding
    This commit enables edge-to-edge display in `MainActivity` and updates padding to accommodate system bars.
    
    - Called `enableEdgeToEdge()` in `MainActivity.defaultActivityCodes()`.
    - Modified `ViewCompat.setOnApplyWindowInsetsListener` in `MainActivity` to set bottom padding to 0, allowing content to draw behind the navigation bar.
    - Removed `android:fitsSystemWindows="true"` from `fragment_add.xml` as it's no longer needed with the edge-to-edge implementation.

--- app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/activity/main/MainActivity.kt
+++ app/src/main/java/com/yusufcanmercan/weight_track_app/ui/view/activity/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.yusufcanmercan.weight_track_app.ui.view.activity.main
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
@@ -63,9 +64,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun defaultActivityCodes() {
         setContentView(binding.root)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
     }
