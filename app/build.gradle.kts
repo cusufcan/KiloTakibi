d9150f82de01598f729e1c0d2d763f7e4d059fe6
cusufcan <cusufcan@gmail.com>
2025-07-19 13:24:04 +0300

    feat: update app version
    This commit updates the application's version information in the `app/build.gradle.kts` file.
    
    - Incremented `versionCode` from 20 to 21.
    - Updated `versionName` from "2.1.0" to "2.2.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.yusufcanmercan.weight_track_app"
         minSdk = 25
         targetSdk = 36
-        versionCode = 20
-        versionName = "2.1.0"
+        versionCode = 21
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
